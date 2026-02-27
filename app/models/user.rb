8bcafecf90e60282ec9cad98c9d3128b874548e7
glgsysyoshimoto <glgsysyoshimoto@gmail.com>
2026-02-27 17:06:16 +0900

    正規表現を修正した

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
 
   with_options presence: true do
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/, message: '全角文字を使用してください', allow_blank: true }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/, message: '全角文字を使用してください', allow_blank: true }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください', allow_blank: true }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください', allow_blank: true }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください', allow_blank: true }
     validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください', allow_blank: true }
   end
@@ -17,20 +15,4 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください', allow_blank: true
-  # validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-
-  # with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/, message: '全角文字を使用してください' } do
-  #   validates :last_name
-  #   validates :first_name
-  # end
-
-  # with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' } do
-  #   validates :last_name_kana
-  #   validates :first_name_kana
-  # end
-
-  # validates :birth_date, presence: true
-
-  # PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  # validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
