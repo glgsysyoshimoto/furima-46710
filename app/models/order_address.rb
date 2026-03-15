05fe8893670bacfaaa87eabede4c0261a2976bad
glgsysyoshimoto <glgsysyoshimoto@gmail.com>
2026-03-15 14:48:19 +0900

    /を\に、collection_selectの第5引数に修正した

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,12 +4,11 @@ class OrderAddress
 
   with_options presence: true do
     validates :postal_code,
-              format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)',
-                        allow_blank: true }
+              format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :house_number
-    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid', allow_blank: true }
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
     validates :user_id
     validates :item_id
     validates :token
